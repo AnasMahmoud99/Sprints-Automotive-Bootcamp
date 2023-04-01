e9ffa17730828171c0f79ed8808d031afff95d78
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-01 19:28:33 +0200

    Update sort.h

--- Sprint2/Refactor previous tasks/LCS/sort.h
+++ Sprint2/Refactor previous tasks/LCS/sort.h
@@ -1,1 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
 
+#include <stdio.h>
+#include <string.h>
+
+int8_t insertionSort(int32_t *array, uint8_t arraySize);
+
+void printArray(int32_t *array, uint8_t arraySize);
+
+#endif
