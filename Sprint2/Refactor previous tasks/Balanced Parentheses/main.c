6bdc43e6686db71c90b1d088fb2ce2a8dc815af4
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-01 22:44:06 +0200

    Update main.c

--- Sprint2/Refactor previous tasks/Balanced Parentheses/main.c
+++ Sprint2/Refactor previous tasks/Balanced Parentheses/main.c
@@ -1,1 +1,24 @@
+#include<main.h>
 
+uint8_t test_balance[10];
+
+	while(1){
+
+		printf("enter the expression \n");
+
+		fflush(stdout);
+
+		scanf("%s",&test_balance);
+
+		int8_t output = isBalancedParanthethes(&test_balance);
+
+		if(output==0)
+			printf("parentheses are balanced\n\n");
+
+		if(output==-1)
+			printf("parentheses are not balanced\n\n");
+
+		if(output== -2)
+			printf("neither of parentheses is used\n\n");
+
+			}
