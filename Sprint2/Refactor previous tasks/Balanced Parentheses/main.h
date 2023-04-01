d7978fb5138e3980c58201fecc137c5425f67d5b
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-01 22:41:18 +0200

    Update main.h

--- Sprint2/Refactor previous tasks/Balanced Parentheses/main.h
+++ Sprint2/Refactor previous tasks/Balanced Parentheses/main.h
@@ -1,1 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define STACK_SIZE 10
 
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Stack.h"
+#include "balanced.h"
+
+#endif
