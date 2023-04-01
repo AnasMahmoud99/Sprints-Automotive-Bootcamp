45b2295f1e5f1b3e4236e65911629af71d7c35e2
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-01 22:47:33 +0200

    Update main.h

--- Sprint2/Refactor previous tasks/LCS/main.h
+++ Sprint2/Refactor previous tasks/LCS/main.h
@@ -5,16 +5,11 @@
 #include <stdlib.h>
 #include <sort.h>
 #include <LCS.h>
+#include <string.h>
 
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
 typedef signed int int32_t;
-
-
-
-
-
-
 
 
 #endif
