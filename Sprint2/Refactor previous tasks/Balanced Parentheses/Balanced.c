c90538eaf651f07038ae72139e17cc7008122194
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-01 22:30:41 +0200

    Update Balanced.c

--- Sprint2/Refactor previous tasks/Balanced Parentheses/Balanced.c
+++ Sprint2/Refactor previous tasks/Balanced Parentheses/Balanced.c
@@ -1,1 +1,37 @@
 
+#include<Balanced.h>
+
+int8_t isBalancedParanthethes(uint8_t *expression){
+
+	ST_stack_t stack_check;
+
+	createEmptyStack(&stack_check);
+
+	uint8_t Top = 0 ;
+
+	for(short i=0 ; i<strlen(expression);i++){
+		getStackTop(&stack_check, &Top);
+
+		if(expression[i]=='(' || expression[i]=='{')
+			push(&stack_check,expression[i]);
+
+		else if(expression[i]==')' || expression[i]=='}'){
+
+			if(stack_check.top == -1)
+				return -1;
+
+			pop(&stack_check,&Top);
+
+		    if((expression[i]==')' && Top!='(') || (expression[i]=='}'&& Top!='{'))
+				return -1;
+			}
+		}
+	if(stack_check.top != -1)
+		return -1;
+
+	else if(Top=='\0')
+		return -2;
+
+	return 0;
+
+	}
