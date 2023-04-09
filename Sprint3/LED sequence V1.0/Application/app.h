ee8eaa567c40e93ab3bd05cbd1c22cc3ec4382a1
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-10 00:58:14 +0200

    Add files via upload

---
+++ Sprint3/LED sequence V1.0/Application/app.h
@@ -0,0 +1,15 @@
+
+
+#ifndef APP_H_
+#define APP_H_
+#define F_CPU	4000000UL
+
+#include "../ECUAL/BUTTON Driver/button.h"
+#include "../ECUAL/LED Driver/led.h"
+#include <util/delay.h>
+void appInit();
+
+void appStart(void);
+
+
+#endif /* APP_H_ */
