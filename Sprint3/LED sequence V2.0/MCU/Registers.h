1c7a31ff2d9cf3de7360ead3d25fd5b2ebf788f0
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-04-12 13:13:09 +0200

    Update Registers.h

--- Sprint3/LED sequence V2.0/MCU/Registers.h
+++ Sprint3/LED sequence V2.0/MCU/Registers.h
@@ -34,5 +34,12 @@
 #define PIND *((volatile uint8_t*)0x30)
 
 
+/*******************************************/
+/*           Interrupt Registers               */
+/*******************************************/
+#define SREG   (* (volatile uint8_t *) (0x5F))
+#define GICR   (* (volatile uint8_t *) (0x5B))
+#define GIFR   (* (volatile uint8_t *) (0x5A))
+#define MCUCR  (* (volatile uint8_t *) (0x55))
 
 #endif /* REGISTERS_H_ */
