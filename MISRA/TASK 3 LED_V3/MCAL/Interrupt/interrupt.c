5f04b2910a94298f245c312b38e98f33d9c189a9
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-06-11 00:59:46 +0300

    Add files via upload

---
+++ MISRA/TASK 3 LED_V3/MCAL/Interrupt/interrupt.c
@@ -0,0 +1,108 @@
+
+
+#include "interrupt.h"
+#include "../../Service/bit_manipulation.h"
+
+
+void Interrupt_Enable(uint8_t ID, uint8_t Mode){
+	
+	Set_Bit(SREG , 7);
+		
+		 if(ID ==EX_INT0 ){
+				Set_Bit(GICR , EX_INT0);
+				
+				if(Mode == RISING_EDGE)
+				{
+					Set_Bit(MCUCR , 0);
+					Set_Bit(MCUCR , 1);	
+				}
+				else if(Mode == FALLING_EDGE)
+				{
+					Set_Bit(MCUCR , 1);
+					Clear_Bit(MCUCR  , 0);
+				}
+				else if(Mode == LOW_LEVEL)
+				{
+					 Clear_Bit(MCUCR , 0);
+					 Clear_Bit(MCUCR , 1);
+				}
+				else if(Mode == ANY_CHANGE)
+				{
+					Set_Bit(MCUCR , 0);
+					Clear_Bit(MCUCR ,1);
+				}
+				else
+				{
+					// Handling Error
+				}
+
+		 }
+				
+		if(ID ==EX_INT1 ){	
+			Set_Bit(GICR , EX_INT1);
+			
+			if(Mode == RISING_EDGE)
+			{
+				Set_Bit(MCUCR , 0);
+				Set_Bit(MCUCR , 1);
+			}
+			else if(Mode == FALLING_EDGE)
+			{
+				Set_Bit(MCUCR , 1);
+				Clear_Bit(MCUCR  , 0);
+			}
+			else if(Mode == LOW_LEVEL)
+			{
+				Clear_Bit(MCUCR , 0);
+				Clear_Bit(MCUCR , 1);
+			}
+			else if(Mode == ANY_CHANGE)
+			{
+				Set_Bit(MCUCR , 0);
+				Clear_Bit(MCUCR ,1);
+			}
+			else
+			{
+				// Handling Error
+			}
+
+		}
+			
+		if(ID ==EX_INT2 ){
+			Set_Bit(GICR , EX_INT2);
+			
+			if(Mode == RISING_EDGE)
+			{
+				Set_Bit(MCUCR , 0);
+				Set_Bit(MCUCR , 1);
+			}
+			else if(Mode == FALLING_EDGE)
+			{
+				Set_Bit(MCUCR , 1);
+				Clear_Bit(MCUCR  , 0);
+			}
+			else if(Mode == LOW_LEVEL)
+			{
+				Clear_Bit(MCUCR , 0);
+				Clear_Bit(MCUCR , 1);
+			}
+			else if(Mode == ANY_CHANGE)
+			{
+				Set_Bit(MCUCR , 0);
+				Clear_Bit(MCUCR ,1);
+			}
+			else
+			{
+				// Handling Error
+			}
+
+		}
+	
+	
+}
+
+void Interrupt_Disable(){
+	
+	Clear_Bit(SREG,7);
+	
+}
