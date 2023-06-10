cd3d3866fc0988977ef4ac06bcb18124055c873b
Anas <67676102+AnasMahmoud99@users.noreply.github.com>
2023-06-10 23:22:52 +0300

    Add files via upload

---
+++ MISRA/TASK 2 Timer/MCAL/Interrupt/interrupt.h
@@ -0,0 +1,39 @@
+
+
+#ifndef INTERRUPT_H_
+#define INTERRUPT_H_
+
+#include "../../Service/Registers.h"
+#include <avr/interrupt.h>
+
+
+
+
+/*   Definitions of External Interrupt IDs   */
+#define EX_INT0	    6
+#define EX_INT1 	7
+#define EX_INT2	    5
+
+/*   Definitions of External Interrupt Modes   */
+#define LOW_LEVEL			0x00
+#define ANY_CHANGE			0x01
+#define FALLING_EDGE		0x10
+#define RISING_EDGE			0x11
+
+// Bit 0,1 – ISC01,ISC00: Interrupt Sense Control 0 Bit0 and Bit1
+#define EXTI_ISC00		0
+#define EXTI_ISC01		1
+
+// Bit 2,3 – ISC11,ISC10: Interrupt Sense Control 1 Bit2 and Bit3
+#define EXTI_ISC10		2
+#define EXTI_ISC11   	3
+
+//  Bit 6 – ISC2: Interrupt Sense Control 2
+#define EXTI_ISC2		6
+
+
+void Interrupt_Enable(uint8_t ID, uint8_t Mode);
+
+void Interrupt_Disable(void);
+
+#endif /* INTERRUPT_H_ */
